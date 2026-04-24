65db81c2489f2935815acc1e86b22258f8d40f9f
shonima <shoni.vinus@gmail.com>
2026-04-24 07:59:19 +0530

    loader file is created

--- src/pages/Joblist.jsx
+++ src/pages/Joblist.jsx
@@ -1,43 +1,44 @@
 
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { fetchJobs } from '../services/Jobservice'
-import JobCard from '../components/Jobcard';
-import '../styles/Joblist.css';
-function Joblist() {
-    const [jobs,setjobs]=useState([]);
-    const [loading,setloading]=useState(true);
-    const [error,seterror]=useState();
-   
-useEffect(() => {
-    fetchJobs()
-    .then((data) =>{
-        setjobs(data);
-        setloading(false);
-    })
-    .catch((error) =>{
-        seterror(error.message);
-        setloading(false);
-    })
+import JobCard from '../components/Jobcard'
+import Loader from '../components/Loader'
+import '../styles/Joblist.css'
 
-},[]);
-if(loading){
-    return <h2>Loading...</h2>
-}
-if(error){
+function Joblist() {
+  const [jobs, setjobs] = useState([])
+  const [loading, setloading] = useState(true)
+  const [error, seterror] = useState()
+
+  useEffect(() => {
+    fetchJobs()
+      .then((data) => {
+        setjobs(data)
+        setloading(false)
+      })
+      .catch((error) => {
+        seterror(error.message)
+        setloading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
     return <h2>Error: {error}</h2>
+  }
+
+  return (
+    <div className='main'>
+      <div className='container'>
+        {jobs.map((j) => (
+          <JobCard key={j.id} job={j} />
+        ))}
+      </div>
+    </div>
+  )
 }
-        
-    return (    
-        <div className='main'>
-            
-            <div className='container'>
-            {
-                jobs.map((j) => (
-                    <JobCard key={j.id} job={j}/>
-                
-            ))}
-            </div>
-        </div>
-    )
-}
-export default Joblist;
+
+export default Joblist
